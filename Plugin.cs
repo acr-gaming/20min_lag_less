e20bc6a1ed512078a918da323ef34cde50fc3f84
ACR <almostcertainlyrealgamer@gmail.com>
2022-07-09 08:53:53 -0700

    random junk

--- Plugin.cs
+++ Plugin.cs
@@ -29,45 +29,56 @@ namespace LagLess
         [HarmonyPatch(typeof(PlayerController))]
         public class PlayerPatch
         {
-            static int howOften = 60;
+            static int experienceLayer = 10;
             static int count = 0;
 
-            [HarmonyPatch("Update")]
-            [HarmonyPrefix]
-            static void Update(PlayerController __instance)
+            [HarmonyPatch("Start")]
+            [HarmonyPostfix]
+            static void Start()
             {
-
-                if (count >= howOften)
-                {
-                    Vector3 playerLocation = __instance.transform.position;
-                    Collider2D[] collisions = Physics2D.OverlapCircleAll(playerLocation, 20);
-
-                    foreach (Collider2D collision in collisions)
-                    {
-                        if (collision.tag == "Pickup")
-                        {
-                            StaticLogger.LogInfo("pickup found");
-
-                            flanne.Pickups.XPPickup xppickup = collision.GetComponent<flanne.Pickups.XPPickup>();
-                            if (xppickup)
-                            {
-                                StaticLogger.LogInfo($"pickup amount {xppickup.amount}");
-                            }
-                        }
-                    }
-
-
-
-                    count = 0;
-                }
-                else
-                {
-                    count++;
-                }
-
-
-
+                Physics2D.IgnoreLayerCollision(0, experienceLayer, true);
+                GameObject PickerUpper = GameObject.FindGameObjectWithTag("Pickupper");
+                PickerUpper.layer = 10;
             }
+
+
+            // [HarmonyPatch("Update")]
+            // [HarmonyPrefix]
+            // static void Update(PlayerController __instance)
+            // {
+            //     int playerLevel = __instance.GetComponentInChildren<flanne.Player.PlayerXP>().level;
+
+
+            //     if (playerLevel >= 60)
+            //     {
+
+
+            //         if (count >= howOften)
+            //         {
+            //             Vector3 playerLocation = __instance.transform.position;
+            //             Collider2D[] collisions = Physics2D.OverlapCircleAll(playerLocation, 100);
+
+            //             foreach (Collider2D collision in collisions)
+            //             {
+            //                 if (collision.tag == "Pickup")
+            //                 {
+            //                     StaticLogger.LogInfo("pickup found");
+
+            //                     flanne.Pickups.XPPickup xppickup = collision.GetComponent<flanne.Pickups.XPPickup>();
+            //                     if (xppickup)
+            //                     {
+            //                         Destroy(xppickup.transform.gameObject);
+            //                     }
+            //                 }
+            //             }
+            //             count = 0;
+            //         }
+            //         else
+            //         {
+            //             count++;
+            //         }
+            //    }
+            //}
 
 
 
